e763127f98a54c7c5aa90be245bb1953ddf6e55f
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-12-11 14:35:14 -0700

    Moved project views to new folder - Created a project view to display all entries

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -5,7 +5,7 @@ module.exports = {
    * Renders the home entry page
    */
   entryHome: (req, res) => {
-    res.render('newEntry');
+    res.render('project/newEntry');
   },
 
   /**
@@ -18,5 +18,27 @@ module.exports = {
       writtenDate: new Date(),
     }).catch(error => console.log('entryController.entryPost error ' + error.message));
     next();
+  },
+
+  /**
+   * Uses .find() to find all entries and places them into the res.locals object
+   * Passes next()
+   */
+  getAllEntries: (req, res, next) => {
+    Entry.find({}).then(entries => {
+      res.locals.entries = entries;
+      next();
+    }).catch(err => {
+      console.log('Error finding entries at entryController.showAllEntries ' + err.message);
+      next(err);
+    }
+    );
+  },
+
+  /**
+   * Renders the project page, which is currently a list of all submissions in order
+   */
+  showAllEntries: (req, res) => {
+    res.render('project/project');
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const errorRoutes = require('./errorRoutes');
 
 router.get('/new-entry', entryController.entryHome);
 router.post('/new-entry', entryController.entryPost, entryController.entryHome);
-
+router.get('/project', entryController.getAllEntries, entryController.showAllEntries);
 // router.use('/', (req, res) => {
 //   res.send('A new route home');
 // });
