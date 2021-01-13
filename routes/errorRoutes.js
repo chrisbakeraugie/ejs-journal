8ac262a02d0078074d25ede39644f54f368b65c5
Chris Baker <chrisbaker@Chriss-MBP.home>
2021-01-13 14:52:50 -0500

    Deleted unused errorRoutes (replaced)

--- routes/errorRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const errorController = require('../controllers/errorController');
-
-// router.use(errorController.pageNotFound);
-// router.use(errorController.internalServerError);
-
-module.exports = router;
