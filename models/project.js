00bfdfcd89e56b4e79a1f7557c5c57cea64ce601
Chris Baker <chrisbaker@Chriss-MBP.home>
2021-02-28 23:18:28 -0500

    title "unique" option removed

--- models/project.js
+++ models/project.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const projectSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
 
   description: {
