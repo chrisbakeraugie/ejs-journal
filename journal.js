f5f61ecb286f912e36265f897206c253dda1d1ff
Chris Baker <chrisbaker@Chriss-MBP.home>
2021-03-25 16:42:22 -0400

    Quick linting issue fix

--- journal.js
+++ journal.js
@@ -133,7 +133,7 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https') {
-      res.redirect(`https://${req.header('host')}${req.url}`)
+      res.redirect(`https://${req.header('host')}${req.url}`);
     } else {
       next();
     }
